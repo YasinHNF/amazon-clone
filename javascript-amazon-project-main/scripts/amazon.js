56d718f538020548ed06b4a65365ae3ccdc73ff6
Yasin <hanifehmohammadyasin@gmail.com>
2026-08-16 16:19:32 +0330

    Moved products into a seperate file

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,43 +1,9 @@
-const products = [
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        // cents
-        price: 1090 
-    },
-
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        price: 2095
-    },
-
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        price: 799
-    }
-];
-
-
 let productsHTML = '';
 
 
 products.forEach(
     (product, index) => {
-        const { image, name, rating, price } = product;
+        const { image, name, rating, priceCents } = product;
 
         const html = `
         <div class="product-container">
@@ -59,7 +25,7 @@ products.forEach(
           </div>
 
           <div class="product-price">
-            $${(price / 100).toFixed(2)}
+            $${(priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
