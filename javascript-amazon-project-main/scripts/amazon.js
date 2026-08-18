33f4a7a24f4282e28a5f7f07b09b5eae50b9c9f7
Yasin <hanifehmohammadyasin@gmail.com>
2026-08-18 18:08:57 +0330

    Deleted the data script tags Made the amazon.js module in HTML Imported products from product.js Imported cart from cart.js

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,5 @@
 import { cart } from '../data/cart.js';
+import { products } from '../data/products.js';
 
 let productsHTML = '';
 const cartQuantityElement = document.querySelector('.js-cart-quantity');
