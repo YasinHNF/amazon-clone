7e84bb345c16cdbee06bddb1f75589af54ed0901
Yasin <hanifehmohammadyasin@gmail.com>
2026-09-11 13:53:11 +0330

    Added backend practice file

---
+++ data/backend-practice.js
@@ -0,0 +1,7 @@
+const xhr = new XMLHttpRequest();
+
+xhr.addEventListener('load', () => console.log(xhr.response))
+
+xhr.open('GET', 'https://www.supersimplebackend.dev');
+
+xhr.send();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import renderProductsSummary from './checkout/orderSummary.js';
 import renderPriceSummary from './checkout/paymentSummary.js';
+import '../data/backend-practice.js';
 // import '../data/car.js';
 // import '../data/cart-class.js';
 
